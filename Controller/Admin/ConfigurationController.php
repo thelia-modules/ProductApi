a2c19cf63217ec90beff1d665e1108e72eb8ad12
Florian BERNARD <Cav0n@hotmail.fr>
2020-02-10 17:08:14 +0100

    Added api key generation

--- Controller/Admin/ConfigurationController.php
+++ Controller/Admin/ConfigurationController.php
@@ -4,7 +4,32 @@
 namespace ProductAPI\Controller\Admin;
 
 
-class ConfigurationController
-{
+use ElasticSearchProduct\ElasticSearchProduct;
+use ProductAPI\ProductAPI;
+use Thelia\Controller\Admin\BaseAdminController;
+use Thelia\Core\HttpFoundation\JsonResponse;
+use Thelia\Core\Security\AccessManager;
+use Thelia\Core\Security\Resource\AdminResources;
 
+class ConfigurationController extends BaseAdminController
+{
+    /**
+     * @return mixed|\Thelia\Core\HttpFoundation\Response
+     */
+    public function viewAction()
+    {
+        if (null !== $response = $this->checkAuth(array(AdminResources::MODULE), array('ProductAPI'), AccessManager::VIEW)) {
+            return $response;
+        }
+
+        return $this->render('productapi/configuration');
+    }
+
+    /**
+     * @return JsonResponse The api key
+     */
+    public function getApiKeyAction()
+    {
+        return JsonResponse::create(ProductAPI::API_KEY, 200);
+    }
 }
